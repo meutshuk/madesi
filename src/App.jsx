6113e57b29d042e972e2aeba96963a9ada74c162
Utshuk Dhamala <me.utshuk1001@outlook.com>
2023-08-25 23:48:15 +1000

    add favicon and change title name

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Footer from "./Footer";
 import Home from "./Home";
 import Mission from "./Mission";
@@ -5,6 +6,32 @@ import Nav from "./Nav";
 import Objectives from "./Objectives";
 
 function App() {
+  useEffect(() => {
+    const faviconEl = document.getElementById("favicon");
+
+    const setFaviconForTheme = () => {
+      const darkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+      if (darkScheme.matches) {
+        faviconEl.href = "/mnf-website-favicon-color.png";
+      } else {
+        faviconEl.href = "/mnf-website-favicon-white.png";
+      }
+    };
+
+    // Set the favicon based on the current theme
+    setFaviconForTheme();
+
+    // Event listener to update the favicon if user changes system theme
+    const darkSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    darkSchemeQuery.addEventListener(setFaviconForTheme);
+
+    // Cleanup listener on component unmount
+    return () => {
+      darkSchemeQuery.removeEventListener(setFaviconForTheme);
+    };
+  }, []); // Empty dependency array to ensure this effect runs once after initial render
+
   return (
     <>
       <Nav />
